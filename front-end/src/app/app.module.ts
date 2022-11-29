472ccb737fe573cfec68761a3125303f9e3fb9d4
SunnyJinny <hyejin.kang@rwth-aachen.de>
2022-11-29 16:22:49 +0100

    Add Components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,30 +5,18 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
 import { HeaderComponent } from './header/header.component';
-<<<<<<< Updated upstream
 import { MainPageNavComponent } from './main-page-nav/main-page-nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { FloatBtnComponent } from 'src/app/float-btn/float-btn.component';
-=======
-import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
->>>>>>> Stashed changes
-
 @NgModule({
   declarations: [
     AppComponent,
     ProductCardListComponent,
     HeaderComponent,
-<<<<<<< Updated upstream
     MainPageNavComponent,
     FooterComponent,
     FloatBtnComponent,
     routingComponents
-=======
-    FooterComponent,
-    routingComponents,
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
