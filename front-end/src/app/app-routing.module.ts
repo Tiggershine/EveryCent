8c9c88f190a43593d22b47a0835212e1787d1bc7
SunnyJinny <hyejin.kang@rwth-aachen.de>
2022-11-29 16:20:42 +0100

    Change app-routing dummy->mainpage

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,11 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { MainPageComponent } from './pages/main-page/main-page.component';
-=======
-<<<<<<< Updated upstream
-import { DummyPageComponent } from './dummy-page/dummy-page.component';
->>>>>>> Stashed changes
 
 const routes: Routes = [
   { path: "", component: MainPageComponent, 
@@ -41,57 +36,10 @@ const routes: Routes = [
     }
   }
 ];
-=======
-import { MainPageComponent } from './pages/main-page/main-page.component'
-
-const routes: Routes = [
-  { path: "", component: MainPageComponent, 
-    data: {
-      // Header-property
-      leftIcon: '../../assets/icons/everycent.svg',
-      rightIconWeb1: '../../assets/icons/loginBtn.svg',
-      rightIconWeb2: '../../assets/icons/signupBtn.svg',
-      rightIconMobile: '../../assets/icons/loginIcon.svg',
-      leftIconLink: 'MainPageComponent',
-      rightIconWeb1Link: 'LoginPageComponent',
-      rightIconWeb2Link: 'SignupPageComponent',
-      rightIconMobileLink: 'LoginPageComponent',
-      rightBtn1Show: true,
-      rightBtn2Show: true,
-      rightBtnMobileShow: true
-
-    }
-  },
-  { path: "login", component: MainPageComponent, 
-    data: {
-      leftIcon: '../../assets/icons/backIcon.svg',
-      rightIconWeb1: '../../assets/icons/loginBtn.svg',
-      rightIconWeb2: '../../assets/icons/member.svg',
-      rightIconMobile: '../../assets/icons/homeIcon.svg',
-      leftIconLink: 'MainPageComponent',
-      rightIconWeb1Link: 'LoginPageComponent',
-      rightIconWeb2Link: 'SignupPageComponent',
-      rightIconMobileLink: 'LoginPageComponent',
-      rightBtn1Show: false,
-      rightBtn2Show: true,
-      rightBtnMobileShow: true
-    }
-  }
-];
-
->>>>>>> Stashed changes
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-<<<<<<< Updated upstream
 export const routingComponents = [ MainPageComponent ];
-=======
-<<<<<<< Updated upstream
-export const routingComponents = [ DummyPageComponent];
-=======
-export const routingComponets = [ MainPageComponent ];
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+
