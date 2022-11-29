eb634b405afa976a9b36f2d69c3c38c99bc14e27
SunnyJinny <hyejin.kang@rwth-aachen.de>
2022-11-29 11:33:31 +0100

    Add sticky feature in Header, Add search bar in Header

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit {
   /**
    *  [ Properties for header-DOM ]
    *  - leftIcon: directory source for left icon on header
@@ -15,12 +14,38 @@ export class HeaderComponent {
    *  - rightIconLink: loaded Component, wenn click on rightIcon
    *  - text: text showed on center header
    */
-  @Input() leftIcon: string;
-  @Input() rightIcon: string;
-  @Input() text: string;
-  @Input() leftIconLink: string;
-  @Input() rightIconLink: string;
-
+   @Input() leftIcon?: string;
+   @Input() rightIconWeb1?: string;
+   @Input() rightIconWeb2?: string;
+   @Input() rightIconMobile?: string;
+   @Input() leftIconLink?: string;
+   @Input() rightIconWeb1Link?: string;
+   @Input() rightIconWeb2Link?: string;
+   @Input() rightIconMobileLink?: string;
+   @Input() rightBtn1Show?: boolean;
+   @Input() rightBtn2Show?: boolean;
+   @Input() rightBtnMobileShow?: boolean;
+ 
+  screenMode: string;
+  headerfixed: boolean = false;
+  @HostListener('window:scroll', ['$event']) onscroll() {
+    if(window.scrollY > 200) {
+      this.headerfixed = true;
+    } else {
+      this.headerfixed = false;
+    }
+  }
   constructor() { }
   
+  ngOnInit(): void {
+    let screenWidth = window.innerWidth;
+    (screenWidth > 767) ? this.screenMode = "web" : this.screenMode = "mobile"
+}
+
+  @HostListener ('window:resize', ['$event'])
+    onResize(event: any) {
+      let screenWidth = window.innerWidth;
+      (screenWidth > 767) ? this.screenMode = "web" : this.screenMode = "mobile"
+      console.log(this.screenMode);
+    }
 }
