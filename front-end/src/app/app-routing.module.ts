7d1ac10aeecd244310dbc5c85cd8b36796fcf5ad
minseon.kim <minseon.kim@rwth-aachen.de>
2022-12-06 19:08:30 +0100

    Add Route for Productdetail and change mockup product

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
 
 const routes: Routes = [
   { path: "", component: MainPageComponent, 
@@ -34,7 +35,24 @@ const routes: Routes = [
       rightBtn2Show: true,
       rightBtnMobileShow: true
     }
-  }
+  },
+  { path: "detail", component: ProductDetailPageComponent, 
+    data: {
+      // Header-property
+      leftIcon: '../../assets/icons/everycent.svg',
+      rightIconWeb1: '../../assets/icons/loginBtn.svg',
+      rightIconWeb2: '../../assets/icons/signupBtn.svg',
+      rightIconMobile: '../../assets/icons/loginIcon.svg',
+      leftIconLink: 'MainPageComponent',
+      rightIconWeb1Link: 'LoginPageComponent',
+      rightIconWeb2Link: 'SignupPageComponent',
+      rightIconMobileLink: 'LoginPageComponent',
+      rightBtn1Show: true,
+      rightBtn2Show: true,
+      rightBtnMobileShow: true
+
+    }
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
