94839ee3665295d416430a8ca2316eb7858452bd
Hyun Kim <kmasterh@gmail.com>
2022-12-09 00:09:16 +0100

    Change_Component_Header#1 add service file, change routin.module, modify html file

---
+++ front-end/src/app/components/header1/header1.component.ts
@@ -0,0 +1,63 @@
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeaderServiceService } from 'src/app/services/header-service.service';
+
+@Component({
+  selector: 'app-header1',
+  templateUrl: './header1.component.html',
+  styleUrls: ['./header1.component.scss']
+})
+
+
+export class Header1Component implements OnInit {
+  @Input() iconLWLink?: string;
+  @Input() iconR1WLink?: string;
+  @Input() iconR2WLink?: string;
+  @Input() iconLMLink?: string;
+  @Input() iconR1MLink?: string;
+  @Input() iconR2MLink?: string;
+
+  routePath: string;  // route path
+  screenMode: string;
+  headerC: any;
+  headerFixed: boolean = false;  
+
+  // iLW: boolean;
+
+  constructor(private route: ActivatedRoute ,private headerService: HeaderServiceService) { 
+  }
+
+  ngOnInit(): void {
+    // get data from routing.module.ts
+    this.routePath = this.route.snapshot.data['path'];
+    
+    // screenMode depends on user screen size 
+    let screenWidth = window.innerWidth;
+    (screenWidth > 767) ? this.screenMode = "W" : this.screenMode = "M";
+    
+    this.headerC = this.headerService.getHeaderContentList().find((arr) => arr.id === this.routePath);
+
+    
+    console.log(this.screenMode);
+    console.log(this.headerC['iconLWSource']);
+
+   
+  }
+
+
+  @HostListener ('window:resize', ['$event'])
+  // On every resizing get the screen size data continuously
+  onResize(event: any) {
+    let screenWidth = window.innerWidth;
+    (screenWidth > 767) ? this.screenMode = "W" : this.screenMode = "M";
+  }
+
+  @HostListener('window:scroll', ['$event']) onscroll() {
+    if(window.scrollY > 200) {
+      this.headerFixed = true;
+    } else {
+      this.headerFixed = false;
+    }
+  }
+
+}
