5af979b0d3ac8f7a6d50786c7727e641798004f8
Hyun Kim <kmasterh@gmail.com>
2022-12-20 22:31:41 +0100

    Set user register, login, Connect MongoDB

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { MainPageNavComponent } from './components/main-page-nav/main-page-nav.c
 import { FooterComponent } from './components/footer/footer.component';
 import { FloatBtnComponent } from 'src/app/components/float-btn/float-btn.component';
 import { HeaderServiceService } from './services/header-service.service';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,13 +24,17 @@ import { HeaderServiceService } from './services/header-service.service';
     FooterComponent,
     FloatBtnComponent,
     routingComponents,
-    HeaderComponent
+    HeaderComponent,
+    SignupPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [HeaderServiceService],
+  providers: [HeaderServiceService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
