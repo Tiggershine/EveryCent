ebf7145337d63b455dcf0f2365baa2becbdd6e58
SunnyJinny <aufkang.hj@gmail.com>
2022-12-25 01:44:46 +0100

    Feature List lazy-loading

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http'
+
 import { AppComponent } from './app.component';
 import { ProductCardListComponent } from './components/product-card-list/product-card-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainPageNavComponent } from './components/main-page-nav/main-page-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FloatBtnComponent } from 'src/app/components/float-btn/float-btn.component';
-import { HeaderServiceService } from './services/header-service.service';
 import { ProductDescriptionComponent } from './components/product-description/product-description.component';
 import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
+
+import { HeaderServiceService } from './services/header-service.service';
 
 @NgModule({
   declarations: [
@@ -27,6 +30,9 @@ import { ProductDetailPageComponent } from './pages/product-detail-page/product-
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [HeaderServiceService],
   bootstrap: [AppComponent]
