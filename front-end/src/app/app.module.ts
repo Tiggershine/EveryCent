571f7b533d060ff78a99fa0abe544bf4c222103a
SunnyJinny <aufkang.hj@gmail.com>
2022-12-25 19:32:05 +0100

    Add all app.module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCardListComponent } from './components/product-card-list/product-card-list.component';
@@ -8,15 +8,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { MainPageNavComponent } from './components/main-page-nav/main-page-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FloatBtnComponent } from 'src/app/components/float-btn/float-btn.component';
-import { HeaderServiceService } from './services/header-service.service';
 import { ProductDescriptionComponent } from './components/product-description/product-description.component';
 import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
-
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
-import { FormsModule } from '@angular/forms';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+import { HeaderServiceService } from './services/header-service.service';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
