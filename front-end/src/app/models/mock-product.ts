cba693206cd348308c6049d3ddba921f207a6dd1
Hanbit <chanbit10@gmail.com>
2023-01-01 23:07:37 +0100

    okay

--- front-end/src/app/models/mock-product.ts
+++ front-end/src/app/models/mock-product.ts
@@ -1,4 +1,4 @@
-import { Product } from "./product";
+import { Product } from './product';
 
 export const PRODUCTS: Product[] = [
   {
@@ -6,22 +6,28 @@ export const PRODUCTS: Product[] = [
     userId: 'Jin',
     productTitle: 'reiskocher',
     productPrice: 80,
-    district: "Aachen Hörn",
+    district: 'Aachen Hörn',
     productCategory: 'electronics',
-    productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id. Tristique nulla aliquet enim tortor at auctor. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Sed blandit libero volutpat sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget. ',
-    productImage: ['../assets/images/cuchen.jpeg','../assets/images/jacket.jpeg','../assets/images/bicycle.jpeg'],
+    productDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id. Tristique nulla aliquet enim tortor at auctor. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Sed blandit libero volutpat sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget. ',
+    productImage: [
+      '../assets/images/cuchen.jpeg',
+      '../assets/images/jacket.jpeg',
+      '../assets/images/bicycle.jpeg',
+    ],
     contact: 'anonym@rwth-aachen.de',
     dealType: 'sell',
     postDate: '24.10.22',
   },
-   {
+  {
     productId: '2',
     userId: 'Hyun',
     productTitle: 'bicycle',
     productPrice: 120,
-    district: "Aachen Hörn",
+    district: 'Aachen Hörn',
     productCategory: 'electronics',
-    productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id. Tristique nulla aliquet enim tortor at auctor. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Sed blandit libero volutpat sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget. ',
+    productDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id. Tristique nulla aliquet enim tortor at auctor. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Sed blandit libero volutpat sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget. ',
     productImage: ['../assets/images/bicycle.jpeg'],
     contact: 'anonym@rwth-aachen.de',
     dealType: 'sell',
@@ -32,14 +38,16 @@ export const PRODUCTS: Product[] = [
     userId: 'Min',
     productTitle: 'jacket',
     productPrice: 40,
-    district: "Aachen Hörn",
+    district: 'Aachen Hörn',
     productCategory: 'electronics',
-    productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id. Tristique nulla aliquet enim tortor at auctor. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Sed blandit libero volutpat sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget. ',
+    productDescription:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id. Tristique nulla aliquet enim tortor at auctor. Montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada. Sed blandit libero volutpat sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Semper feugiat nibh sed pulvinar. Morbi tincidunt ornare massa eget. ',
     productImage: ['../assets/images/jacket.jpeg'],
     contact: 'anonym@rwth-aachen.de',
     dealType: 'sell',
     postDate: '24.10.22',
   },
+
   {
     productId: '4',
     userId: 'hyun',
@@ -168,4 +176,4 @@ export const PRODUCTS: Product[] = [
     productImage: ['../assets/images/microwave.jpeg'],
     postDate: '15.11.22',
   },
-]
+];
