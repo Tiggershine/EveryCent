bd8a04d6ede66871d6943dc7a28c37d3b6cde3b8
Hanbit <chanbit10@gmail.com>
2023-01-02 08:28:34 +0100

    routing register page 23 with connecting db

--- front-end/src/app/components/product-information/product-information.component.ts
+++ front-end/src/app/components/product-information/product-information.component.ts
@@ -1,5 +1,8 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { json } from 'body-parser';
+import { Product } from 'src/app/models/product';
+import { ProductServiceService } from 'src/app/services/product-service.service';
 
 @Component({
   selector: 'app-product-information',
@@ -8,6 +11,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class ProductInformationComponent implements OnInit {
   tradeOptionRadioButton = ['sell', 'buy'];
+
   informationType = [
     {
       sellOption: 'I want',
@@ -27,31 +31,81 @@ export class ProductInformationComponent implements OnInit {
   ];
 
   districtType = [
-    { place: 'Aachen Mitte' },
-    { place: 'Brand' },
-    { place: 'Eilendorf' },
-    { place: 'Haarem' },
-    { place: 'Kornelimünster Walheim' },
-    { place: 'Laurensberg' },
-    { place: 'Richterich' },
+    'Markt',
+    'Theater',
+    'Lindenplatz',
+    'St. Jakob',
+    'Westpark',
+    'Kronenberg',
+    'Hörn',
+    'Ponttor',
+    'Hansemannplatz',
+    'Soers',
+    'Jülicher Straße',
+    'Kalkofen',
+    'Kaiserplatz',
+    'Adalbertsteinweg',
+    'Panneschopp',
+    'Rothe Erde',
+    'Forst',
+    'Frankenberger Viertel',
+    'Burtscheid',
+    'Marschiertor',
+    'Beverau',
   ];
 
-  productForm!: FormGroup;
+  product: Product = {
+    productId: '',
+    productTitle: 'productTitleWorks',
+    productPrice: 123213,
+    district: '',
+    productCategory: '',
+    productDescription: 'descriptionWorks',
+    productImage: ['232332'],
+    contact: '',
+    dealType: 'buy',
+    postDate: '',
+  };
+
+  // title,
+  // description,
+  // price,
+  // imageUrl,
+  // user,
+  // district,
+  // dealType,
+
+  // productId!: string;
+  // userId?: string;
+  // productTitle!: string;
+  // productPrice!: number;
+  // district?: string;
+  // productCategory?: string;
+  // productDescription?: string;
+  // productImage!: string[];
+  // contact?: string;
+  // dealType?: string;
+  // postDate!: string;
+
+  //productForm!: FormGroup;
   screenMode: string;
   imageData!: string;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductServiceService
+  ) {}
 
   ngOnInit(): void {
-    this.productForm = this.formBuilder.group({
-      productName: ['', Validators.required],
-      category: ['', Validators.required],
-      date: ['', Validators.required],
-      tradeOption: ['', Validators.required],
-      price: ['', Validators.required],
-      comment: ['', Validators.required],
-      district: ['', Validators.required],
-    });
+    // this.productForm = this.formBuilder.group({
+    //   productName: ['', Validators.required],
+    //   category: ['', Validators.required],
+    //   date: ['', Validators.required],
+    //   tradeOption: ['', Validators.required],
+    //   price: ['', Validators.required],
+    //   comment: ['', Validators.required],
+    //   district: ['', Validators.required],
+    // });
 
     let screenWidth = window.innerWidth;
     screenWidth > 767
@@ -67,18 +121,40 @@ export class ProductInformationComponent implements OnInit {
       : (this.screenMode = 'mobile');
     console.log(this.screenMode);
   }
-  addProduct() {}
 
-  onFileSelect(event: Event) {
-    const file = (event.target as HTMLInputElement).files?.[0];
-    this.productForm.patchValue({ image: file });
-    const allowedMimeTypes = ['image/png', 'image/jpeg', 'image/jpg'];
-    if (file && allowedMimeTypes.includes(file.type)) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.imageData = reader.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
+  addProduct(): void {
+    const data = {
+      title: this.product.productTitle,
+      description: this.product.productDescription,
+      price: this.product.productPrice,
+      imageUrl: this.product.productImage,
+      user: {
+        email: 'hanbit@gmail.com',
+        password: 'love0324465',
+        username: 'hanbitt',
+      },
+      district: this.product.district,
+      dealType: this.product.dealType,
+    };
+
+    this.productService.create(data).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err) => console.log(err),
+    });
   }
+
+  // onFileSelect(event: Event) {
+  //   const file = (event.target as HTMLInputElement).files?.[0];
+  //   this.productForm.patchValue({ image: file });
+  //   const allowedMimeTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+  //   if (file && allowedMimeTypes.includes(file.type)) {
+  //     const reader = new FileReader();
+  //     reader.onload = () => {
+  //       this.imageData = reader.result as string;
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // }
 }
---
+++ front-end/src/app/models/constants/urls.ts
@@ -0,0 +1,3 @@
+const BASE_URL = 'http://localhost:3000';
+
+export const PRODUCT_CARD_REGISTER = BASE_URL + '/card/register';
