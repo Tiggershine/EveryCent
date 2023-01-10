129af0a991fd9261d0e0f52a12b6b4441e269806
Hanbit <chanbit10@gmail.com>
2023-01-10 20:54:38 +0100

    10.01 product Card mjs

--- front-end/server/api/routes/productCard.mjs
+++ front-end/server/api/routes/productCard.mjs
@@ -1,55 +1,39 @@
-import express from "express";
+import express from 'express';
 const router = express.Router();
-import * as productCardRepository from "../models/productCard.model.mjs";
+import * as productCardRepository from '../models/productCard.model.mjs'
 import storage from "../middleware/upload.js";
 
 // Get all cards
-router.get("/cards", async (req, res) => {
+router.get('/cards', async (req, res) => {
   const productCards = await productCardRepository.getAll();
   if (productCards) {
     res.status(200).json(productCards);
   } else {
     res.status(404).json({
-      message: "product-cards are not found",
+      message: 'product-cards are not found'
     });
   }
-});
+})
 
 // Get product cards
-router.get("/card/:id", async (req, res) => {
+router.get('/card/:id', async(req, res) => {
   const id = req.params.id;
   const product = await productCardRepository.getProductCard(id);
-  if (product) {
+  if(product) {
     res.status(200).json(product);
   } else {
-    res.status(404).json({ message: "product not found" });
+    res.status(404).json({ message: 'product not found' });
   }
-});
+
+
+})
 
 // Create a card
-router.post("/register", async (req, res) => {
-  const {
-    title,
-    description,
-    price,
-    category,
-    imageUrl,
-    user,
-    district,
-    dealType,
-  } = req.body;
-  const productCard = await productCardRepository.createCard(
-    title,
-    description,
-    price,
-    category,
-    imageUrl,
-    user,
-    district,
-    dealType
-  );
+router.post('/register', async (req, res) => {
+  const { title, description, price, category, imageUrl, user, district, dealType } = req.body;
+  const productCard = await productCardRepository.createCard(title, description, price, category, imageUrl, user, district, dealType);
   res.status(201).json(productCard);
-});
+})
 
 router.post("/register/image", storage, async (req, res) => {
   console.log(req.body);
@@ -57,47 +41,35 @@ router.post("/register/image", storage, async (req, res) => {
   res.json({ message: "Successfully uploaded files" });
 });
 
+
 // Update the card
-router.put("/update/:id", async (req, res) => {
-  const {
-    title,
-    description,
-    price,
-    category,
-    imageUrl,
-    user,
-    district,
-    dealType,
-  } = req.body;
+router.put('/update/:id', async (req, res) => {
+  const { title, description, price, category, imageUrl, user, district, dealType } = req.body;
   const id = req.params.id;
   const prodcutCard = await productCardRepository.getProductCard(id);
   if (!prodcutCard) {
     res.status.json({
-      message: `can not find product-card with ${id}`,
+      message: `can not find product-card with ${id}`
     });
   }
-  const updated = await productCardRepository.updateCard(
-    title,
-    description,
-    price,
-    category,
-    imageUrl,
-    user,
-    district,
-    dealType
-  );
+  const updated = await productCardRepository.updateCard(title, description, price, category, imageUrl, user, district, dealType);
   res.status(200).json(updated);
-});
+})
+
 
 // Delete the card
-router.delete("/delete/:id", async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
   const id = req.params.id;
   const productCard = await productCardRepository.getProductCard(id);
   if (!productCard) {
-    res.status(404).json({ message: `can not find product-card with ${id}` });
+    res.status(404).json({ message: `can not find product-card with ${id}`});
   }
   await productCardRepository.removeCard(id);
   res.sendStatus(204);
-});
+})
+
+
+
+
 
 export default router;
